fe765b98836519681100f12a6b9c31a790b336b9
MessageGit <message-box@outlook.fr>
2021-03-13 15:20:11 +0100

    remove <title> and font link (Google API)

--- groupomania-front-end/src/App.vue
+++ groupomania-front-end/src/App.vue
@@ -1,14 +1,6 @@
 <template>
-  <link rel="preconnect" href="https://fonts.gstatic.com">
-  <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200&family=Ubuntu:wght@300&display=swap" rel="stylesheet">
   <router-view/>
 </template>
-
-<script>
-export default {
-  created() { document.title = "Groupomania - Parlez de tout et de rien entre collègues !"; }
-}
-</script>
 
 <style lang="scss">
 #app {
