4b1fdaa086b24967a42152059fa34ec4ad7a3076
MessageGit <message-box@outlook.fr>
2021-03-13 14:24:43 +0100

    Regex validation added for login system

--- groupomania-front-end/src/components/loginModal.vue
+++ groupomania-front-end/src/components/loginModal.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="auth-form"><!-- Connexion -->
-    <div class="input-block">
-      <input type="text" name="username" class="input-form" v-model="formData.email" id="sign-username"/>
-      <label for="sign-username" :class="{ 'focus-label' : formData.email }">Nom d'utilisateur</label>
+  <form class="auth-form" @submit.prevent="formSubmit()"><!-- Connexion -->
+    <div class="input-block" :class="{ 'input-error' : formData.error == 1 }" @click="formData.error = 0">
+      <input type="text" name="email" class="input-form" v-model="formData.email" id="login-email"/>
+      <label for="login-username" :class="{ 'focus-label' : formData.email }">Votre e-mail</label>
     </div>
-    <div class="input-block">
-      <input type="password" name="passwrd" class="input-form" v-model="formData.passwrd" id="sign-passwrd" />
-      <label for="sign-passwrd" :class="{ 'focus-label' : formData.passwrd }">Mot de passe</label>
+    <div class="input-block" :class="{ 'input-error' : formData.error == 2 }" @click="formData.error = 0">
+      <input type="password" name="passwrd" class="input-form" v-model="formData.passwrd" id="login-passwrd" />
+      <label for="login-passwrd" :class="{ 'focus-label' : formData.passwrd }">Mot de passe</label>
     </div>
     <button class="submit-button">Connexion</button>
-  </div>
+  </form>
 </template>
 
 <script>
@@ -17,7 +17,16 @@ export default {
   name: 'loginModal',
   data() {
     return {
-      formData: { email: '', passwrd: '' }
+      formData: { email: '', passwrd: '', error: 0 }
+    }
+  },
+  methods: {
+    formSubmit() {
+      if(this.formData.email && /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(this.formData.email)) {
+        if(this.formData.passwrd && this.formData.passwrd.length > 5) {
+          alert('ok');
+        } else { this.formData.error = 2 }
+      } else { this.formData.error = 1 }
     }
   }
 }
