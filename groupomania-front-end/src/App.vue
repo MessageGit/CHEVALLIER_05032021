e0e5a9c81f48560f3b92d2bd657682294fe5b683
MessageGit <message-box@outlook.fr>
2021-03-13 14:13:28 +0100

    unused CSS deleted

--- groupomania-front-end/src/App.vue
+++ groupomania-front-end/src/App.vue
@@ -6,9 +6,7 @@
 
 <script>
 export default {
-  created() {
-    document.title = "Groupomania - Parlez de tout et de rien entre collègues !";
-  }
+  created() { document.title = "Groupomania - Parlez de tout et de rien entre collègues !"; }
 }
 </script>
 
@@ -19,18 +17,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
